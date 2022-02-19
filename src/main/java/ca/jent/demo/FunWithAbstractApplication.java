dcc8111b8bbb8c69942fb1aa9accee39ee722e40
Jocelyn Raymond <it.jraymond@gmail.com>
2022-02-19 15:36:03 -0700

    Initial setup

---
+++ src/main/java/ca/jent/demo/FunWithAbstractApplication.java
@@ -0,0 +1,13 @@
+package ca.jent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunWithAbstractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunWithAbstractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/jent/demo/FunWithAbstractApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jent.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunWithAbstractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
